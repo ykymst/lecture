d8485f8a3dcd7376e05187b2e5d3a96c64c5ad3c
山下優樹 <yukiyama913@gmail.com>
2020-07-01 09:52:08 +0900

    organize in function

--- statistical_machine_learning/02/homework3/homework3.py
+++ statistical_machine_learning/02/homework3/homework3.py
@@ -1,19 +1,25 @@
 import numpy as np
 
-# load data
-train_list = []
-test_list = []
-for i in range(10):
-    train_list.append(np.loadtxt('data/digit_train{}.csv'.format(i), delimiter=','))
-    test_list.append(np.loadtxt('data/digit_test{}.csv'.format(i), delimiter=','))
-train = np.stack(train_list)
-test = np.stack(test_list)
+def load_data():
+    train_list = []
+    test_list = []
+    for i in range(10):
+        train_list.append(np.loadtxt('data/digit_train{}.csv'.format(i), delimiter=','))
+        test_list.append(np.loadtxt('data/digit_test{}.csv'.format(i), delimiter=','))
+    train = np.stack(train_list)
+    test = np.stack(test_list)
+    return train, test
 
-# estimate
-mean = train.mean(axis = 1).T
-cov = np.array([np.cov(array.T) for array in train]).mean(axis = 0)
-cov_inv = np.linalg.inv(cov)
-result = (mean.T.dot(cov_inv).dot(test.transpose(1, 2, 0)).T - mean.T.dot(cov_inv).dot(mean / 2).diagonal()).argmax(axis = 2)
+def estimate(train, test):
+    mean = train.mean(axis = 1).T
+    cov = np.array([np.cov(array.T) for array in train]).mean(axis = 0)
+    cov_inv = np.linalg.inv(cov)
+    result = (mean.T.dot(cov_inv).dot(test.transpose(1, 2, 0)).T - mean.T.dot(cov_inv).dot(mean / 2).diagonal()).argmax(axis = 2)
+    return result
 
-# show result
-print((np.tile(np.arange(10).T, (200, 10, 1)).T == result).sum(axis = 2))
+def show_result(result):
+    return (np.tile(np.arange(10).T, (200, 10, 1)).T == result).sum(axis = 2)
+
+train, test = load_data()
+result = estimate(train, test)
+print(show_result(result))
